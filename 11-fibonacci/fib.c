f5122559191567e67a78b18d0b15228bba39ac4e
megaVE <viniherofactory@gmail.com>
2023-09-30 19:30:09 -0300

    11th exercise

--- 11-fibonacci/fib.c
+++ 11-fibonacci/fib.c
@@ -32,23 +32,20 @@ char * sum(char * a, char * b, char * aux){ // sums two numbers stored as string
         strcpy(b, aux);
     }
 
-    // printf("start: %s + %s\n", a, b);
     int buffer = 0;
     for(int i=major-1; i >= 0; i--){ // sums each number digit
         int aux_sum = (a[i] - '0') + (b[i] - '0') + buffer;
-        // printf("a %c | b %c | aux_sum[%d]: %d\n", a[i], b[i], i, aux_sum);
         aux[i+1] = aux_sum % 10 + '0';
         if(aux_sum > 9)
             buffer = 1;
         else
             buffer = 0;
     }
-    if(buffer)
+    if(buffer) // last buffer checking
         aux[0] = '1';
     else
         aux[0] = '0';
     aux[major+1] = '\0';
-    // printf("aux mid result: %s\n", aux);
     
     if(aux[0] == '0'){ // removes the first empty digit
         for(int i=0; i < strlen(aux)-1; i++){
@@ -60,11 +57,7 @@ char * sum(char * a, char * b, char * aux){ // sums two numbers stored as string
     return aux;
 }
 
-char * subtract(char * a, char * b, char * aux){ // subtracts two numbers stored as strings
-    // TODO
-} 
-
-char * greater(char * a, char * b){ // compares two numbers stored as strings
+char * greater(char * a, char * b){ // compares two numbers stored as strings and returns the greater one
     if(strlen(a) > strlen(b)) // a has more digits
         return a;
 
@@ -82,25 +75,43 @@ char * greater(char * a, char * b){ // compares two numbers stored as strings
     return a; // a equals b
 }
 
-// int fibonacci(int number){ // Fibonacci recursive function
-//     return (number < 3 ? 1 : fibonacci(number - 1) + fibonacci(number - 2));
-// }
+char * fibonacci(int number, char * aux){ // Fibonacci recursive function
+    char aux1[100] = "1";
+    char aux2[100] = "1";
+    
+    for(int i=2; i <= number; i++){
+        sum(aux1, aux2, aux); // aux = aux1 + aux2;
+        strcpy(aux1, aux2); // aux1 = aux2;
+        strcpy(aux2, aux); // aux2 = aux;
+    }
+
+    strcpy(aux, aux2);
+    return aux;
+}
 
 int main(){
-    int result = 0;
+    char table[MAX_FIBONACCI][MAX_LENGTH+1];
+    strcpy(table[0], "1"); // table[0] = 1;
+    int table_length = 1;
 
-    char a[MAX_LENGTH], b[MAX_LENGTH], aux[MAX_LENGTH];
-    for(scanf("%s %s", a, b); a[0] != '0' || b[0] != '0'; scanf("%s %s", a, b)){
-        printf("%s %s\n", a, b);
-        printf("greater value: %s\n", greater(a, b));
-        printf("%s + %s = %s\n", a, b, sum(a, b, aux));
-        // char fib[100] = fibonacci(1);
-        // for(int i=2; fib < b; i++){
-            // if(fib >= a)
-                // result++;
-            // fib = fibonacci(i);
-        // }
-        // printf("%d\n", result);
+    char aux[MAX_LENGTH+1];
+    char a[MAX_LENGTH+1], b[MAX_LENGTH+1];
+    for(scanf("%s %s", a, b); a[0] != '0' || b[0] != '0'; scanf("%s %s", a, b)){ // Input
+        int result = 0;
+                
+        for(; !strcmp(greater(table[table_length-1], b), b); table_length++) // if(b >= table[table_length-1]) | Fills the Fibonacci's Table when needed
+            strcpy(table[table_length], fibonacci(table_length+1, aux)); // table[table_length] = fibonacci(table_length+1, aux);
+
+        for(int i=0; ; i++){ // Calculates the number of Fibonaccis between a and b
+            if(!strcmp(greater(table[i], a), table[i])){ // if(table[i] >= a)
+                if(!strcmp(greater(table[i], b), table[i])) // if(table[i] >= b)
+                    if(strcmp(table[i], b)) // if(table[i] != b)
+                        break;
+                result++;
+            }
+        }
+
+        printf("%d\n", result);
     }
     return 0;
 }
